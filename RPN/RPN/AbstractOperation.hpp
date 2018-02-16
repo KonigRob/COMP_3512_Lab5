c468502f3e77cbc840d4bce72ba4599e55367ae8
Rob Koenig <rkoenig5@my.bcit.ca>
2018-02-16 10:14:45 -0800

    AbstractOperation.hpp

---
+++ RPN/RPN/AbstractOperation.hpp
@@ -0,0 +1,12 @@
+#pragma once
+#include "Operation.hpp"
+
+class AbstratOperation : public Operation
+{
+	char operation_type;
+
+public:
+	AbstratOperation(char sign) : operation_type(sign) {};
+	char get_code() override {return operation_type;}
+	virtual ~AbstratOperation() {}
+};
